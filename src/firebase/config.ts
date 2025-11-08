2d400432dbaa4aa663d75cc41dfa43d2f0018dd5
Momntous Lyfe <momntouslyfe@gmail.com>
2025-11-08 10:33:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6914152784-d6441",
+  "appId": "1:666100698197:web:f962b2d7d83a52eb8218dc",
+  "apiKey": "AIzaSyCfqpqhZfrUiF9uR2QaUi5_moBNnjcCwQA",
+  "authDomain": "studio-6914152784-d6441.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "666100698197"
+};
